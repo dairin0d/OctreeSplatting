34acaccc32aa3c590ef6922d976ebfb103340148
dairin0d <dima.glib@gmail.com>
2024-04-06 15:40:44 +0300

    Display the frame time too

--- Godot/OctreeSplatting/FpsCounter.cs
+++ Godot/OctreeSplatting/FpsCounter.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class FpsCounter : Label {
 	public override void _Process(double deltaTime) {
-		Text = Engine.GetFramesPerSecond().ToString();
+		var fps = Engine.GetFramesPerSecond();
+		var ms = (int)Math.Ceiling(1000f / fps);
+		Text = $"FPS: {fps} ({ms} ms)";
 	}
 }
