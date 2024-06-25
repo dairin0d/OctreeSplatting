52c85a25ee99a5023115e0d1d9a3000d237124ca
dairin0d <dima.glib@gmail.com>
2024-06-25 11:59:43 +0300

    Display non-accumulated total FPS too

--- Godot/OctreeSplatting/FpsCounter.cs
+++ Godot/OctreeSplatting/FpsCounter.cs
@@ -6,6 +6,6 @@ public partial class FpsCounter : Label {
 	public override void _Process(double deltaTime) {
 		var fps = Engine.GetFramesPerSecond();
 		var ms = (int)Math.Ceiling(1000f / fps);
-		Text = Timing.Report;
+		Text = Timing.Report+$"\nFPS:{fps} ({ms} ms)";
 	}
 }
